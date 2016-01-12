52997eb439d9326d7bbb01de618e2ae49fb94454
Michael Lor and Sean Peerenboom <seanmp42@gmail.com>
2016-01-12 11:48:50 -0800

    complete js and index.html

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,11 @@
 $(document).ready(function() {
+$("form#letter1").submit(function(event) {
+    var americainput = $("input#america").val();
 
-  $("button#cat").click(function() {
-  $("img#catimg").show();
-  $("ul#ctext").prepend("<li id='catsays'>Cats will rule!</li>");
-  $("li#catsays:eq(1)" ).remove();
+     $(".america").text(americainput);
+
+    event.preventDefault();
+
+    $("#letter").show();
   });
-
-  $("button#dog").click(function() {
-  $("img#dogimg").show();
-  $("ul#dtext").prepend("<li id='dogsays'>Dogs are awesome!</li>");
-  $("li#dogsays:eq(1)" ).remove();
   });
-
-
-
-
-  // $(function).click(){
-  //
-  // }
-
-
-
-});
